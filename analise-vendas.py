cb195ae6c9ab3264fc0753408a03230558e43b1b
Hiago Alves <36165242+devHiagoAlves@users.noreply.github.com>
2026-02-05 12:37:05 -0300

    Update analise-vendas.py

--- analise-vendas.py
+++ analise-vendas.py
@@ -1,5 +1,20 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+# Lendo direto do GitHub
 url = "https://raw.githubusercontent.com/devHiagoAlves/analise-vendas-python/main/vendas.csv"
 dados = pd.read_csv(url)
-print(dados)
+
+# Criando a coluna de valor total
+dados["Valor_total"] = dados["Quantidade"] * dados["Preco_unitario"]
+
+# Gráfico de barras
+dados.groupby("Vendedor")["Valor_total"].sum().plot(kind="bar", color="skyblue")
+plt.title("Total vendido por vendedor")
+plt.ylabel("Valor em R$")
+plt.xlabel("Vendedor")
+plt.show()
+
+dados.groupby("Produto")["Quantidade"].sum().plot(kind="pie", autopct="%1.1f%%") 
+plt.title("Participação de cada produto nas vendas") 
+plt.ylabel("") # remove o rótulo lateral plt.show()
